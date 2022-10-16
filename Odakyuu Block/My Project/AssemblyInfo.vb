ac791e9754355b3da5f2684efd22dc8cc24d7165
Yami An <yamiannephilim@gmail.com>
2022-10-16 20:53:14 +0700

    init solution

---
+++ Odakyuu Block/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Resources
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Odakyuu Block")>
+<Assembly: AssemblyDescription("小田急ブロック")>
+<Assembly: AssemblyCompany("YAN")>
+<Assembly: AssemblyProduct("Odakyuu Block")>
+<Assembly: AssemblyCopyright("Copyright © Yami An")>
+<Assembly: AssemblyTrademark("Nephilim")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("3e94b339-13e3-4914-ba26-a18425a2346d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("22.10.16.0")>
+<Assembly: AssemblyFileVersion("22.10.16.0")>
+<Assembly: NeutralResourcesLanguage("ja")>
---
+++ README.md
@@ -0,0 +1,7 @@
+# ODAKYUU BLOCK SOLUTION
+Solution to help 西山 team of エマール group calculate ブロック of 小田急 from 文化シャッター partner.
+
+## MASK
+<p align="center">
+<img src="https://raw.githubusercontent.com/Tynab/Mitsubishi-Block/main/pic/0.png"></img>
+</p>
